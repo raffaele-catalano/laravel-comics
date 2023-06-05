5630a8fda48df8f599d86978a353fda07b8fbe5b
raffaele-catalano <raf.catalano@gmail.com>
2023-06-05 19:29:41 +0200

    implemented db-menu

---
+++ config/db-menu.php
@@ -0,0 +1,92 @@
+<?php
+
+return [
+    'menuHeader' => [
+        [
+            'label' => 'characters',
+            'href'  => 'home'
+        ],
+        [
+            'label' => 'comics',
+            'href'  => 'comics'
+        ],
+        [
+            'label' => 'movies',
+            'href'  => 'movies'
+        ],
+        [
+            'label' => 'tv',
+            'href'  => 'tv'
+        ],
+        [
+            'label' => 'games',
+            'href'  => 'games'
+        ],
+        [
+            'label' => 'collectibles',
+            'href'  => 'collectibles'
+        ],
+        [
+            'label' => 'videos',
+            'href'  => 'videos'
+        ],
+        [
+            'label' => 'fans',
+            'href'  => 'fans'
+        ],
+        [
+            'label' => 'news',
+            'href'  => 'news'
+        ],
+        [
+            'label' => 'shop',
+            'href'  => 'shop'
+        ]
+    ],
+///////////////////////////////////////////////////////////////////////////////
+    'menuCTA' => [
+        [
+            'label' => 'digital comics',
+            'src'   => '/resources/assets/img/buy-comics-digital-comics.png'
+        ],
+        [
+            'label' => 'dc merchandise',
+            'src'   => '/resources/assets/img/buy-comics-merchandise.png'
+        ],
+        [
+            'label' => 'subscriptions',
+            'src'   => '/resources/assets/img/buy-comics-subscriptions.png'
+        ],
+        [
+            'label' => 'comic shop locator',
+            'src'   => '/resources/assets/img/buy-comics-shop-locator.png'
+        ],
+        [
+            'label' => 'dc power visa',
+            'src'   => '/resources/assets/img/buy-dc-power-visa.svg'
+        ],
+    ],
+///////////////////////////////////////////////////////////////////////////////
+    'menuSocial' => [
+        [
+            'label' => 'facebook',
+            'src'   => '/resources/assets/img/footer-facebook.png'
+        ],
+        [
+            'label' => 'twitter',
+            'src'   => '/resources/assets/img/footer-twitter.png'
+        ],
+        [
+            'label' => 'youtube',
+            'src'   => '/resources/assets/img/footer-youtube.png'
+        ],
+        [
+            'label' => 'pinterest',
+            'src'   => '/resources/assets/img/footer-pinterest.png'
+        ],
+        [
+            'label' => 'periscope',
+            'src'   => '/resources/assets/img/footer-periscope.png'
+        ]
+    ]
+];

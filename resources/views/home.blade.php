f7d7a8747a74566e5017819e943059728c3feefd
raffaele-catalano <raf.catalano@gmail.com>
2023-06-05 17:37:02 +0200

    added favicon + minor fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    {{-- Favicon --}}
+    <link rel="shortcut icon" href="dc_favicon.ico" type="ico">
 
     <title>DC Comics</title>
 

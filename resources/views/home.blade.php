7263e8db5a445a0028fbe006413e9587b381d1a9
raffaele-catalano <raf.catalano@gmail.com>
2023-06-06 10:01:03 +0200

    added CTA section + changed font-family

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,23 @@
 
 @section('content')
 
-<h2 class="fw-bold">Home</h2>
+@php
+    $ctaItems = config('db-menu.menuCTA')
+@endphp
+
+<h2>Home</h2>
+
+<div class="cta">
+    <div class="container">
+        <ul>
+            @foreach ($ctaItems as $item)
+            <li>
+                <img src="{{ Vite::asset($item['src']) }}" alt="{{ $item['label'] }}">
+                <span>{{ $item['label'] }}</span>
+            </li>
+            @endforeach
+        </ul>
+    </div>
+</div>
 
 @endsection

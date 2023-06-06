3fd6714baec3b7dfa0e70f9642e04e1c9d233ff8
raffaele-catalano <raf.catalano@gmail.com>
2023-06-06 14:46:04 +0200

    implemented main content with cards added dinamically

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,22 +3,47 @@
 @section('content')
 
 @php
-    $ctaItems = config('db-menu.menuCTA')
+    $comics     = config('db-comics');
+    $ctaItems   = config('db-menu.menuCTA')
 @endphp
 
-<h2>Home</h2>
+{{-- @dd($comics) --}}
 
-<div class="cta">
-    <div class="container">
-        <ul>
-            @foreach ($ctaItems as $item)
-            <li>
-                <img src="{{ Vite::asset($item['src']) }}" alt="{{ $item['label'] }}">
-                <span>{{ $item['label'] }}</span>
-            </li>
-            @endforeach
-        </ul>
+    <div class="main-wrapper">
+        <div class="container">
+            <h4>Current Series</h4>
+            <div class="cards-container">
+
+                @foreach ($comics as $comic)
+                    <div class="card">
+                        <div class="image-container">
+                            <img src="{{ $comic['thumb'] }}" alt="comic-thumb">
+                        </div>
+
+                        <div class="txt-container">
+                            <span>{{ $comic['title'] }}</span>
+                        </div>
+                    </div>
+                @endforeach
+
+            </div>
+            <button>Load More</button>
+        </div>
     </div>
-</div>
+
+    <div class="cta">
+        <div class="container">
+            <ul>
+                @foreach ($ctaItems as $item)
+                <li>
+                    <img src="{{ Vite::asset($item['src']) }}" alt="{{ $item['label'] }}">
+                    <span>{{ $item['label'] }}</span>
+                </li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
+
+
 
 @endsection
